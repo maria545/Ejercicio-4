a28cc20543e9d4bd11e2a3f70524ea23429ef616
Maria <Maria@DESKTOP-G07TP2O>
2020-10-20 17:13:23 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio 4/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Ejercicio_4
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            contadorTextBlock.Text = (textoTextBox.Text.Length) + "/140";
+            if(textoTextBox.Text.Length >= 140)
+            {
+                textoTextBox.IsReadOnly = true;
+            }
+        }
+    }
+}
